042b01322046e713702b17af0caa3bb706c651c0
siobe34 <siobe14@hotmail.com>
2022-12-29 00:00:43 -0500

    configured FontAwesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core";
+
+
+// * Configuring Font Awesome
+fontAwesomeConfig.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
