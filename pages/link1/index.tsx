be11c3d9c91347ce75b063c6843374a8a7e02406
siobe34 <siobe14@hotmail.com>
2022-12-29 00:06:34 -0500

    template pages

---
+++ pages/link1/index.tsx
@@ -0,0 +1,13 @@
+import { PageSection } from "../../components/PageSection";
+
+export default function Link1() {
+    return (
+        <PageSection className='mt-4'>
+            <h1 className='text-2xl font-bold underline'>Page 1</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sunt inventore sed porro soluta accusantium, voluptatem eius,
+                nobis beatae illo cumque est neque odit laboriosam, necessitatibus sint consequuntur ad dolor!
+            </p>
+        </PageSection>
+    );
+}
