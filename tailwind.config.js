437ef49ee7a3043a352b6e9455bd363280e3e0ae
siobe34 <siobe14@hotmail.com>
2022-12-29 00:02:30 -0500

    configured themeing with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
+// * Function to retain the opacity value for elements using custom theme variables
+function withOpacity(cssVariable) {
+    return ({ opacityValue }) => {
+        if (opacityValue !== undefined) {
+            return `rgba(${cssVariable}, ${opacityValue})`;
+        }
+        return `rgb(${cssVariable})`;
+    };
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                bg1: withOpacity("var(--bg1)"),
+                bg2: withOpacity("var(--bg2)"),
+                bg3: withOpacity("var(--bg3)"),
+                txtOnBg: withOpacity("var(--txtOnBg)"),
+                primary: withOpacity("var(--primary)"),
+                txtOnPrimary: withOpacity("var(--txtOnPrimary)"),
+                secondary: withOpacity("var(--secondary)"),
+                txtOnSecondary: withOpacity("var(--txtOnSecondary)"),
+            },
+        },
+    },
+    plugins: [],
+};
