fb98c750a1e6c0524fbef20ab1d36b1147bff398
siobe34 <siobe14@hotmail.com>
2022-12-29 01:48:51 -0500

    basic population of About page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,12 +2,21 @@ import { PageSection } from "../../components/PageSection";
 
 export default function About() {
     return (
-        <PageSection className='mt-4'>
-            <h1 className='text-2xl font-bold underline'>About this Project</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sunt inventore sed porro soluta accusantium, voluptatem eius,
-                nobis beatae illo cumque est neque odit laboriosam, necessitatibus sint consequuntur ad dolor!
-            </p>
+        <PageSection className='justify-start mt-4 px-8'>
+            <article className='flex flex-col gap-4'>
+                <h1 className='flex self-center text-2xl font-bold underline'>What is Landmarks?</h1>
+                <p>
+                    A world map of cool landmarks and respective information. It's an interactive way to learn about interesting geography, weird
+                    archeological facts, various points of interest, and any other intriguing geospatial data.
+                </p>
+                <p>
+                    Just head over to the{" "}
+                    <a href='/map' className='px-2 py-1 rounded bg-secondary text-txtOnSecondary hover:underline'>
+                        Map page
+                    </a>{" "}
+                    and explore the markers on the map.
+                </p>
+            </article>
         </PageSection>
     );
 }
