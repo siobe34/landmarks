36173a2398340e7b37d616ee84866c4bed139267
siobe34 <siobe14@hotmail.com>
2022-12-29 01:22:20 -0500

    blank Map Page

---
+++ pages/map/index.tsx
@@ -0,0 +1,9 @@
+import { RenderMap } from "../../components/Map/RenderMap";
+
+export default function Map() {
+    return (
+        <section className='flex w-full h-full'>
+            <RenderMap />
+        </section>
+    );
+}
