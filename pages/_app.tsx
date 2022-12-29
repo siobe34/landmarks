05ffa11fcdd1e94ecb4d78354b6a7ae9b8cb1769
siobe34 <siobe14@hotmail.com>
2022-12-29 00:06:02 -0500

    adding Layout component to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,17 @@ import type { AppProps } from "next/app";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core";
 
+import { Layout } from "../components/Layout";
 
 // * Configuring Font Awesome
 fontAwesomeConfig.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
