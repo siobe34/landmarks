8d829f5a61b60656d869306b2eed62519c965636
siobe34 <siobe14@hotmail.com>
2022-12-30 16:42:46 -0500

    state & callback for mapPreferences from Storage

--- components/Map/RenderMap.tsx
+++ components/Map/RenderMap.tsx
@@ -1,9 +1,44 @@
+import { useState, useEffect, useCallback } from "react";
+
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+import { IMapPreference } from "../../types/IMap";
+import { ILandmark } from "../../types/ILandmark";
+import { mapPreferences } from "../../utils/mapPreferences";
 
 import { Map } from "./Map";
 import { Marker } from "./Marker";
 
+const DEFAULT_MAP_PREFERENCES = {
+    centre: {
+        lat: 43.7427794,
+        lng: -79.5286326,
+    },
+    zoom: 8,
+};
+
 export const RenderMap = () => {
+    //* Initialize the centre and zoom of the map, and set to default values
+    const [centre, setCentre] = useState<IMapPreference["centre"]>(DEFAULT_MAP_PREFERENCES.centre);
+    const [zoom, setZoom] = useState<IMapPreference["zoom"]>(DEFAULT_MAP_PREFERENCES.zoom);
+
+    const setMapCentreZoom = useCallback(() => {
+        //* Get map preferences from localStorage
+        const storedMapPreferences = mapPreferences.get();
+
+        //* Set map centre if found in localStorage
+        if (storedMapPreferences?.mapLat && storedMapPreferences?.mapLng)
+            setCentre({ lat: storedMapPreferences.mapLat, lng: storedMapPreferences.mapLng });
+
+        //* Set map zoom if found in localStorage
+        if (storedMapPreferences?.mapZoom) setZoom(storedMapPreferences.mapZoom);
+    }, [setCentre, setZoom]);
+
+    //* Run once on page load to set map centre and zoom
+    useEffect(() => {
+        setMapCentreZoom();
+    }, [setMapCentreZoom]);
+
     const handleMapStatus = (status: Status) => {
         switch (status) {
             default:
@@ -15,8 +50,7 @@ export const RenderMap = () => {
                 return <div>Failed to load...</div>;
             case Status.SUCCESS:
                 return (
-                    <Map centre={{ lng: -79.998372, lat: 43.454564 }} zoom={10}>
-                        <Marker position={{ lng: -79.998372, lat: 43.454564 }} />
+                    <Map centre={centre} zoom={zoom}>
                     </Map>
                 );
         }
