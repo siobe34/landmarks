0e7f26810cdaedb043a5c714277502008005a18f
siobe34 <siobe14@hotmail.com>
2022-12-30 16:40:12 -0500

    type for mapPreferences & type for data to Markers

--- types/IMap.ts
+++ types/IMap.ts
@@ -1,4 +1,6 @@
-export interface IMap {
+import { ILandmark } from "./ILandmark";
+
+export interface IMap extends google.maps.MapOptions {
     className?: string;
     children:
         | React.ReactElement<any, string | React.JSXElementConstructor<any>>
@@ -7,7 +9,13 @@ export interface IMap {
     zoom: number;
 }
 
-export interface IMarker {
+export interface IMarker extends google.maps.MarkerOptions {
     map?: google.maps.Map | null | undefined;
     position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined;
+    landmark: ILandmark;
+}
+
+export interface IMapPreference {
+    centre: google.maps.LatLng | google.maps.LatLngLiteral;
+    zoom: number;
 }
