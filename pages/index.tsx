bbf29a7f1f3c74b6192dbcc5474045dfed4fcc06
siobe34 <siobe14@hotmail.com>
2022-12-30 16:34:14 -0500

    changed document title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <title>Create Next App</title>
+                <title>Landmarks</title>
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
