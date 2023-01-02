9a53cfaa7743fa961e19a5dd47b4717cc9d194b1
siobe34 <siobe14@hotmail.com>
2023-01-02 00:42:17 -0500

    cleaning up types

--- types/IMap.ts
+++ types/IMap.ts
@@ -1,6 +1,11 @@
 import { ILandmark } from "./ILandmark";
 
-export interface IMap extends google.maps.MapOptions {
+export interface IMapPreference extends google.maps.MapOptions {
+    centre: google.maps.LatLng | google.maps.LatLngLiteral;
+    zoom: number;
+}
+
+export interface IMap extends IMapPreference {
     className?: string;
     children:
         | React.ReactElement<any, string | React.JSXElementConstructor<any>>
@@ -14,9 +19,4 @@ export interface IMarker extends google.maps.MarkerOptions {
     position: google.maps.LatLng | google.maps.LatLngLiteral | null | undefined;
     displayMarker: boolean;
     landmark: ILandmark;
-}
-
-export interface IMapPreference {
-    centre: google.maps.LatLng | google.maps.LatLngLiteral;
-    zoom: number;
 }
--- utils/MAP_DEFAULTS.ts
+++ utils/MAP_DEFAULTS.ts
@@ -1,9 +1,13 @@
-export const DEFAULT_MAP_PREFERENCES = {
+import { IMapPreference } from "../types/IMap";
+import { ILayerState } from "../types/ILayerState";
+
+export const DEFAULT_MAP_PREFERENCES: IMapPreference = {
     centre: { lat: 0, lng: 0 },
     zoom: 2.5,
 };
 
-export const DEFAULT_LAYER_STATE = {
+
+export const DEFAULT_LAYER_STATE: ILayerState = {
     Archaeology: true,
     Biology: true,
     Geology: true,
