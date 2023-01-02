5efb7aba78a90c1c38c35a325519328c548b39bc
siobe34 <siobe14@hotmail.com>
2023-01-01 23:46:12 -0500

    overriding Button component style

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,21 +4,21 @@ import { IButton } from "../types/IButton";
 
 const buttonUtils = `inline-flex justify-center items-center py-2 px-4 border border-secondary hover:border-primary rounded text-txtOnPrimary hover:text-txtOnSecondary bg-primary hover:bg-secondary cursor-pointer`;
 
-export const Button = ({ className, children, onClick, href, hrefInternal, ariaLabel }: IButton) => {
+export const Button = ({ className, styleOverride, children, onClick, href, hrefInternal, ariaLabel }: IButton) => {
     if (hrefInternal)
         return (
-            <Link className={`${className} ${buttonUtils}`} href={hrefInternal} onClick={onClick} aria-label={ariaLabel}>
+            <Link className={`${className} ${buttonUtils}`} style={styleOverride} href={hrefInternal} onClick={onClick} aria-label={ariaLabel}>
                 {children}
             </Link>
         );
     if (href)
         return (
-            <a className={`${className} ${buttonUtils}`} href={href} onClick={onClick} aria-label={ariaLabel}>
+            <a className={`${className} ${buttonUtils}`} style={styleOverride} href={href} onClick={onClick} aria-label={ariaLabel}>
                 {children}
             </a>
         );
     return (
-        <button className={`${className} ${buttonUtils}`} onClick={onClick} aria-label={ariaLabel}>
+        <button className={`${className} ${buttonUtils}`} style={styleOverride} onClick={onClick} aria-label={ariaLabel}>
             {children}
         </button>
     );
--- types/IButton.ts
+++ types/IButton.ts
@@ -1,5 +1,6 @@
 export interface IButton {
     className?: string;
+    styleOverride?: React.CSSProperties;
     children: React.ReactNode;
     onClick?: React.MouseEventHandler;
     href?: string;
