e0df1279b787f93aa5416c2f20e64186786f4ab2
siobe34 <siobe14@hotmail.com>
2023-01-01 23:50:35 -0500

    moved map defaults to separate file

---
+++ utils/MAP_DEFAULTS.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_MAP_PREFERENCES = {
+    centre: { lat: 0, lng: 0 },
+    zoom: 2.5,
+};
+
+export const DEFAULT_LAYER_STATE = {
+    Archaeology: true,
+    Biology: true,
+    Geology: true,
+    History: true,
+};
