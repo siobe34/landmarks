bc3a4460acefe4f452cd7ab4cb12df0369ae430b
siobe34 <siobe14@hotmail.com>
2023-01-02 17:28:32 -0500

    populated About page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,15 +1,18 @@
 import Head from "next/head";
+import Link from "next/link";
 
 import { PageSection } from "../../components/PageSection";
+
+const linkUtils = "px-2 py-1 rounded bg-secondary text-txtOnSecondary hover:underline";
 
 export default function About() {
     return (
         <>
             <Head>
-                <title>Landmarks | Home</title>
+                <title>Landmarks | About</title>
             </Head>
 
-            <PageSection className='justify-start mt-4 px-8'>
+            <PageSection className='justify-start gap-12 mt-4 px-8'>
                 <article className='flex flex-col gap-4'>
                     <h1 className='flex self-center text-2xl font-bold underline'>What is Landmarks?</h1>
                     <p>
@@ -18,10 +21,19 @@ export default function About() {
                     </p>
                     <p>
                         Just head over to the{" "}
-                        <a href='/' className='px-2 py-1 rounded bg-secondary text-txtOnSecondary hover:underline'>
+                        <Link href='/' className={linkUtils}>
                             Map
-                        </a>{" "}
+                        </Link>{" "}
                         and explore the various markers.
+                    </p>
+                </article>
+                <article className='flex flex-col gap-4'>
+                    <p>
+                        <a href='https://snazzymaps.com/' className={linkUtils}>
+                            Snazzy Maps
+                        </a>{" "}
+                        was used to get the different map themes. Snazzy Maps provides a large variety of map styles to use with your own Google Maps
+                        application.
                     </p>
                 </article>
             </PageSection>
