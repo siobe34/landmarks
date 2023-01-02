c38c05678ca10b27893a1fe9f96d732c73c15234
siobe34 <siobe14@hotmail.com>
2023-01-02 03:07:57 -0500

    basic animation & selecting layers with icons only

--- components/Map/MapLayers.tsx
+++ components/Map/MapLayers.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLayerGroup, faOtter, faVolcano, faDigging, faTimeline } from "@fortawesome/free-solid-svg-icons";
 
 import { IMapLayers } from "../../types/IMapLayers";
 import { ILayerState } from "../../types/ILayerState";
 
-import { Button } from "../../components/Button";
+import { Button, buttonUtils } from "../../components/Button";
 
 export const MapLayers = ({ setLayerState, setMapPreferences }: IMapLayers) => {
+    // * Initialize state of dropdown with default set to false
     const [dropdownDisplay, setDropdownDisplay] = useState<boolean>(false);
 
     // * Func to toggle layer state of markers on map
@@ -22,23 +24,23 @@ export const MapLayers = ({ setLayerState, setMapPreferences }: IMapLayers) => {
     };
 
     return (
-        <div className='absolute top-2 flex flex-col justify-center items-center gap-1 bg-transparent' aria-label='map-layers'>
-            <Button aria-label='Filter Map by Layer' onClick={() => setDropdownDisplay(!dropdownDisplay)}>
+        <div className='flex flex-col justify-center items-center gap-1 bg-transparent' aria-label='map-layers'>
+            <motion.button className={buttonUtils} aria-label='Filter Map by Layer' onClick={() => setDropdownDisplay(!dropdownDisplay)} layout>
                 <FontAwesomeIcon icon={faLayerGroup} className='mr-2' /> Filter
-            </Button>
+            </motion.button>
             {dropdownDisplay && (
                 <ul className='flex flex-col gap-1 text-sm'>
                     <Button styleOverride={{ justifyContent: "flex-start" }} onClick={() => toggleLayers("Archaeology")}>
-                        <FontAwesomeIcon className='mr-2' icon={faDigging} /> Archaeology
+                        <FontAwesomeIcon className='text-2xl' icon={faDigging} />
                     </Button>
                     <Button styleOverride={{ justifyContent: "flex-start" }} onClick={() => toggleLayers("Biology")}>
-                        <FontAwesomeIcon className='mr-2' icon={faOtter} /> Biology
+                        <FontAwesomeIcon className='text-2xl' icon={faOtter} />
                     </Button>
                     <Button styleOverride={{ justifyContent: "flex-start" }} onClick={() => toggleLayers("Geology")}>
-                        <FontAwesomeIcon className='mr-2' icon={faVolcano} /> Geology
+                        <FontAwesomeIcon className='text-2xl' icon={faVolcano} />
                     </Button>
                     <Button styleOverride={{ justifyContent: "flex-start" }} onClick={() => toggleLayers("History")}>
-                        <FontAwesomeIcon className='mr-2' icon={faTimeline} /> History
+                        <FontAwesomeIcon className='text-2xl' icon={faTimeline} />
                     </Button>
                 </ul>
             )}
